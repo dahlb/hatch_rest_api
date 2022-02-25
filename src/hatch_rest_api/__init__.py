3d104ba4cc04c6023de7d012b9f4d5cb7b7c3125
Brendan Dahl <dahl.brendan@gmail.com>
2022-02-25 12:57:46 -0500

    feat: fix library initialization

--- src/hatch_rest_api/__init__.py
+++ src/hatch_rest_api/__init__.py
@@ -1,2 +1,5 @@
+from .aws_http import AwsHttp
 from .errors import BaseError, RateError, AuthError
 from .hatch import Hatch
+from .rest_mini import RestMini
+from .util_bootstrap import get_rest_minis
