9f85b485c4b6c758bd1ab7ff38bf166f32ba21dd
Brendan Dahl <dahl.brendan@gmail.com>
2022-02-26 12:30:14 -0500

    feat: publish tracks

--- src/hatch_rest_api/__init__.py
+++ src/hatch_rest_api/__init__.py
@@ -3,3 +3,4 @@ from .errors import BaseError, RateError, AuthError
 from .hatch import Hatch
 from .rest_mini import RestMini
 from .util_bootstrap import get_rest_minis
+from .const import RestMiniAudioTrack, REST_MINI_AUDIO_TRACKS
