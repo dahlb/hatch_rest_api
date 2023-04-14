9cbf6a36fa834213ca8cea7d32a6ee7a751bb06b
Brendan Dahl <dahl.brendan@gmail.com>
2023-04-13 23:33:28 -0400

    chore: workflow improvements

---
+++ src/hatch_rest_api/__init__.py
@@ -0,0 +1,14 @@
+from .aws_http import AwsHttp
+from .errors import BaseError, RateError, AuthError
+from .hatch import Hatch
+from .rest_mini import RestMini
+from .rest_plus import RestPlus
+from .riot import RestIot
+from .restoreiot import RestoreIot
+from .util_bootstrap import get_rest_devices
+from .const import (
+    RestMiniAudioTrack,
+    REST_MINI_AUDIO_TRACKS,
+    RestPlusAudioTrack,
+    REST_PLUS_AUDIO_TRACKS,
+)
